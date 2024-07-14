ec015a07eec65f1fa957bf3ce2ccba5b7e3a7922
arashPQ <arashpaqe@gmail.com>
2024-07-14 19:07:18 +0330

    """ first commit """

---
+++ Settings.py
@@ -0,0 +1,14 @@
+Operators = {
+
+    
+}
+
+Colors = {
+    'light-gray': {'fg': ('#505050', '#D4D4D2'), 'hover': ('#686868','#efefed'), 'text':('white', 'black')},
+    'dark-gray': {'fg': ('#D4D4D2', '#505050'), 'hover': ('#efefed','#686868'), 'text':('black', 'white')},
+    'orange': {'fg': '#FF9500', 'hover': '#ffb143', 'text':('white', 'black')},
+    'orange-highlight': {'fg': '#white', 'hover': '#white', 'text':('black', '#FF9500')}
+}
+
+Black = '#000000'
+White = '#EEEEEE'
